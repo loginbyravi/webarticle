37377387ea40e1baf403c5b2f2da7f66189733ee
loginbyravi <loginbyravi@gmail.com>
2020-06-14 22:15:49 +0530

    This is first commit

---
+++ src/main/java/in/rkant/webarticle/WebarticleApplication.java
@@ -0,0 +1,13 @@
+package in.rkant.webarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebarticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebarticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/rkant/webarticle/WebarticleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rkant.webarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebarticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
